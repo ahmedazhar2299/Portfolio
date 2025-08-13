65ac38443df3d57631c9363aa38129f9152f39c4
ahmedazhar2299 <ahmed.azhar2299@gmail.com>
2025-08-12 23:27:02 -0700

    Update | Portfolio building blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
